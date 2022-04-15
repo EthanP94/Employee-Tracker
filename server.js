1dc32406195d6f6074f5281d296059f09ad4300b
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-04-15 14:52:49 -0700

    had removed double code overlooked before. I also fixed update employee function

--- server.js
+++ server.js
@@ -159,49 +159,6 @@ function addEmployee() {
   })
 }
 
-function addEmployee() { 
-    inquirer.prompt([
-        {
-          name: "firstname",
-          type: "input",
-          message: "Enter their first name "
-        },
-        {
-          name: "lastname",
-          type: "input",
-          message: "Enter their last name "
-        },
-        {
-          name: "role",
-          type: "list",
-          message: "What is their role? ",
-          choices: selectRole()
-        },
-        {
-            name: "choice",
-            type: "rawlist",
-            message: "Whats their managers name?",
-            choices: selectManager(),
-        }
-    ]).then(function (val) {
-      var roleId = selectRole().indexOf(val.role) + 1
-      var managerId = selectManager().indexOf(val.choice) + 1
-      connection.query("INSERT INTO employee SET ?", 
-      {
-          first_name: val.firstName,
-          last_name: val.lastName,
-          manager_id: managerId,
-          role_id: roleId
-          
-      }, function(err){
-          if (err) throw err
-          console.table(val)
-          startPrompt()
-      })
-
-  })
-}
-
 function updateEmployee() {
     connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
      if (err) throw err
@@ -229,12 +186,14 @@ function updateEmployee() {
         var roleId = selectRole().indexOf(val.role) + 1
         connection.query(
           "UPDATE employee SET ? WHERE ?", 
-        [{
+        [ 
+          {
+            role_id: roleId
+          },
+          {
           last_name: val.lastName
         }, 
-        {
-          role_id: roleId
-        }], 
+        ], 
         function(err){
             if (err) throw err
             console.table(val)
